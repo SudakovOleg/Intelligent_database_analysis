12873e30286c3216065cf544bffd4fe99ae23080
Sudakov Oleg <Sudakov.oleg123@yandex.ru>
2021-05-29 15:03:49 +0300

    Kohonen Network Accelerated Learning

--- kahonen.py
+++ kahonen.py
@@ -12,17 +12,22 @@ class KahononNetwork(object):
     def print_weights(self):
         print(self.weights)
     
-    def output_calc(self, input_table):
+    def train_auto_output(self, input_table):
         self.output_set(int(len(input_table)/2))
         clusters_in_use = {i: False for i in range(self.output_n)}
-        for data in self.train(input_table):
+        new_table = self.train(input_table, len(input_table) * 1000)
+        for data in new_table:
             clusters_in_use[int(data[-1])] = True
         index_in_use = 0
         for key in clusters_in_use:
             if clusters_in_use[key] == True:
+                for data in new_table:
+                    if key == int(data[-1]):
+                        data[-1] = index_in_use
                 index_in_use = index_in_use + 1
-        return self.output_set(index_in_use)
-    
+        self.output_set(index_in_use)
+        return new_table
+
     def output_set(self, output_n):
         self.output_n = output_n
         self.weight = np.random.rand(self.input_n,self.output_n)
@@ -32,7 +37,7 @@ class KahononNetwork(object):
         print(self.input_n)
         print(self.output_n)
     
-    def train(self, input_table):
+    def train(self, input_table , epohs):
         n = len(input_table)
         alpha = 1 # Коэфициент обучения
         #Нулевой вектор длиной в количество даннх
@@ -45,7 +50,7 @@ class KahononNetwork(object):
         print("Total number of features: ",self.input_n)
         print("Total number of Clusters: ",self.output_n)
         print("\nThe initial self.weight: \n", np.round(self.weight,2))
-        for it in range(n * 1000): # Количество итераций
+        for it in range(epohs): # Количество итераций
              list_of_index = list(range(n))
              random.shuffle(list_of_index)
              for i in list_of_index: # Для каждого вектора из перемешанных данных
@@ -101,6 +106,5 @@ class KahononNetwork(object):
                     
             for data in output_v:
                 data[i] = data[i] / maximum
-        print(output_v)
         return output_v
         
